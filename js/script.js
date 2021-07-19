c3c9924a0f8fd41899a7dfb487abd140ce7424af
Rugaroo <63457972+Rugaroo@users.noreply.github.com>
2021-07-20 00:21:42 +0300

    20.07

--- js/script.js
+++ js/script.js
@@ -12,3 +12,28 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
+
+$('.c-carousel').slick({
+  dots: false,
+  infinite: true,
+  arrows: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
